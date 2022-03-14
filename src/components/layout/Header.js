e86279f3aa3b7ca14fd1aac3a9824250aad84880
Keith Tan <keithtanzihao@gmail.com>
2022-03-14 20:09:59 +0800

    Header: Inital header implementation

---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "../ui/navbar/Navbar";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <Navbar />
+      </header>
+    )
+  }
+}
