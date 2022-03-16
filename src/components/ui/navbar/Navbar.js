ea37156985b4cdcd948f52d4b67dc99d724d6ee8
Keith Tan <keithtanzihao@gmail.com>
2022-03-16 13:29:25 +0800

    Navbar: Moved titleList, added NavbarMenu, NavbarBrand, NavButton

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -2,33 +2,20 @@ import React from "react";
 
 import NavbarMenu from "./NavbarMenu";
 import NavbarBrand from "./NavbarBrand";
-import NavbarItem from "./NavbarItem";
+import NavbarButton from "./NavbarButton";
+
+import styles from "../../../styles/main.module.css";
 
 export default class Navbar extends React.Component {
-  
-  renderSectionTitles() {
-    let sectionTitles = [];
-    let titlesList = [
-      "Articles",
-      "Meditation",
-      "Sleep",
-      "Stress",
-      "Mindfulness",
-    ];
-    
-    for (let title of titlesList) {
-      let item = <NavbarItem key={title} sectionTitle={title}/>
-      sectionTitles.push(item);
-    }
-    return sectionTitles;
-  }
 
   render() {
     return (
-      <nav>
-        <NavbarMenu/>
-        <NavbarBrand/>
-        {this.renderSectionTitles()};
+      <nav className={`${styles["navbar"]}`}>
+        <div className={`${styles["navbar__sectionCtn"]}`}>
+          <NavbarMenu setIsMenuOpen={this.props.setIsMenuOpen}/>
+          <NavbarBrand />
+        </div>
+        <NavbarButton />
       </nav>
     );
   }
