fdcc03dc936ff576fa2588e1d672bb8fcb5c1e5b
Keith Tan <keithtanzihao@gmail.com>
2022-03-22 22:44:17 +0800

    App: Added React router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Header from './components/layout/Header';
-import Menu from './components/ui/menu/Menu';
+import Navbar from "./components/ui/navbar/Navbar";
+import Menu from "./components/ui/menu/Menu";
+import Frontpage from "./components/layout/Frontpage";
 
-import './styles/main.module.scss';
+import "./styles/main.module.scss";
 
 export default class App extends React.Component {
-
   state = {
     isMenuOpen: false,
-  }
+  };
 
   setIsMenuOpen = () => {
     this.setState({
-      isMenuOpen: !this.state.isMenuOpen
-    })
-  }
+      isMenuOpen: !this.state.isMenuOpen,
+    });
+  };
 
   renderContent = () => {
     if (this.state.isMenuOpen) {
-      return <Menu />
+      return <Menu />;
     } else {
-      // return <Menu />
+      return <Frontpage />;
     }
-  }
+  };
 
   render() {
     return (
       <React.Fragment>
-        {/* Need to substitude this for react context */}
-        <Header setIsMenuOpen={this.setIsMenuOpen}/>
-        {this.renderContent()}
+        {/* Common components here */}
+        <Navbar setIsMenuOpen={this.setIsMenuOpen} />
+
+        <Route path="/main">
+          {/* Need to substitude this for react context */}
+          {this.renderContent()}
+        </Route>
+
+        <Route path="/login">
+          <h1>LOGIN PAGE</h1>
+        </Route>
+        
       </React.Fragment>
-    )
+    );
   }
 }
-
