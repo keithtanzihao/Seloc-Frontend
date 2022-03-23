cebda650d216bca65c7f387962b8a48930b2492c
Keith Tan <keithtanzihao@gmail.com>
2022-03-23 23:02:59 +0800

    Navbar: Added link to Login page

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import NavbarMenu from "./NavbarMenu";
 import NavbarBrand from "./NavbarBrand";
-import NavbarButton from "./NavbarButton";
+import Button from "../button/Button";
 
 import styles from "../../../styles/main.module.css";
 
 export default class Navbar extends React.Component {
-
   render() {
     return (
       <nav className={`${styles["navbar"]}`}>
         <div className={`${styles["navbar__sectionCtn"]}`}>
-          <NavbarMenu setIsMenuOpen={this.props.setIsMenuOpen}/>
+          <NavbarMenu setIsMenuOpen={this.props.setIsMenuOpen} />
           <NavbarBrand />
         </div>
-        <NavbarButton />
+
+        <NavLink to="/login">
+          <Button class={`${styles["navbtn"]}`} content="Login" />
+        </NavLink>
       </nav>
     );
   }
