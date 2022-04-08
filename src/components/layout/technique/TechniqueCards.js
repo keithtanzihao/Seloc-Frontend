44fc16e3093764cc90da441ecebfdf5dab5e6c1a
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:21:41 +0800

    TechniqueCards: Re-implemented display cards

--- src/components/layout/technique/TechniqueCards.js
+++ src/components/layout/technique/TechniqueCards.js
@@ -1,19 +1,47 @@
-import React from "react";
-
-import PlaceholderImg from "../../ui/svg/PlaceholderImg";
+import React, { Fragment } from "react";
 
 import styles from "../../../styles/main.module.scss";
 
+export default class TechniqueCards extends React.Component {
+  techniqueSelected = () => {
+    this.props.updateStateObjects("isDisplayOpen", true);
+    this.props.updateStateObjects("selectedInfo", this.props.techniqueInfo);
+  };
 
-export default class TechniqueCards extends React.Component {  
+  renderCategories = () => {
+    return this.props.techniqueInfo.category.map(function (element, index) {
+      return (
+        <Fragment key={`${element}${index}`}>
+          <span>{element}</span>{" "}
+        </Fragment>
+      );
+    });
+  };
+
   render() {
+    const { techniqueInfo } = this.props;
+
     return (
       <React.Fragment>
-        <div className={`${styles["technique__card"]}`}>
-          <PlaceholderImg className={`${styles["technique__cardSvg"]}`}/>
-          <h1>{this.props.title}</h1>
+        <div
+          className={`${styles["technique__card"]}`}
+          onClick={this.techniqueSelected}
+        >
+          <img src={techniqueInfo.image} />
+
+          <div className={`${styles["technique__cardCtn"]}`}>
+            <h1>{techniqueInfo.title}</h1>
+
+            <div className={`${styles["technique__cardCategory"]}`}>
+              {this.renderCategories()}
+            </div>
+
+            <p>Difficulty: {techniqueInfo.difficulty}</p>
+            <p>Duration: Approx {techniqueInfo.duration} hours</p>
+          </div>
+          
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
