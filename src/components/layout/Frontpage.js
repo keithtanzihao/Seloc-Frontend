285e81b1de43a1acc95955f04a5c741fe2a7070b
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:17:22 +0800

    Frontpage: Minor changes

--- src/components/layout/Frontpage.js
+++ src/components/layout/Frontpage.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-import FrontpageSignup from "./fontpage/FrontpageSignup";
-
 import styles from "../../styles/main.module.scss";
 
 export default class Frontpage extends React.Component {
   render() {
     return (
       <main className={`${styles["frontpg"]}`}>
-        <FrontpageSignup />
       </main>
     );
   }
