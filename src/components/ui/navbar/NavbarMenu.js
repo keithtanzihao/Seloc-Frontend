cb06b989b7fbd2324e9eee2a19f2bba70d136a64
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:34:34 +0800

    NavbarMenu: Trivial changes

--- src/components/ui/navbar/NavbarMenu.js
+++ src/components/ui/navbar/NavbarMenu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { motion, useAnimation } from "framer-motion";
 
 import styles from "../../../styles/main.module.css";
