c78deb382411da1daf7c36fdd92f046205eac82f
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:33:50 +0800

    Navbar: Re-implemented navbar, added links to pages using react-router

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -2,23 +2,49 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import NavbarMenu from "./NavbarMenu";
-import NavbarBrand from "./NavbarBrand";
-import Button from "../button/Button";
 
 import styles from "../../../styles/main.module.css";
+import loginImg from "../../../styles/vendors/imgs/login/loginImg.png";
+
+import { IconContext } from "react-icons";
+import {
+  BsFillHouseDoorFill,
+  BsFillBookFill,
+  BsFillPersonFill,
+} from "react-icons/bs";
 
 export default class Navbar extends React.Component {
+
+  componentDidMount = () => {
+    
+  }
+
   render() {
     return (
       <nav className={`${styles["navbar"]}`}>
-        <div className={`${styles["navbar__sectionCtn"]}`}>
-          <NavbarMenu setIsMenuOpen={this.props.setIsMenuOpen} />
-          <NavbarBrand />
-        </div>
+        <div className={`${styles["navbar__ctn"]}`}>
+          <h1 className={`${styles["navbar__title"]}`}>BetterLife</h1>
+          <ul className={`${styles["navbar__list"]}`}>
+            <IconContext.Provider
+              value={{ className: `${styles["navbar__icons"]}` }}
+            >
+              <NavLink to="/techniques">
+                <li className={`${styles["navbar__item"]}`}>
+                  <BsFillBookFill />
+                  Library
+                </li>
+              </NavLink>
+              <NavLink to="/login">
+                <li className={`${styles["navbar__item"]}`}>
+                  <BsFillPersonFill />
+                  Login
+                </li>
+              </NavLink>
+            </IconContext.Provider>
 
-        <NavLink to="/login">
-          <Button class={`${styles["navbtn"]}`} content="Login" />
-        </NavLink>
+            {/* <NavbarMenu setIsMenuOpen={this.props.setIsMenuOpen} /> */}
+          </ul>
+        </div>
       </nav>
     );
   }
