d5881960c14004a06fceb60270418924a15f707a
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:27:39 +0800

    menu/Menu: Temporary changes

--- src/components/ui/menu/Menu.js
+++ src/components/ui/menu/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink, withRouter, Redirect } from "react-router-dom";
 
 import Arrow from "../svg/Arrow";
 
@@ -6,20 +7,22 @@ import styles from "../../../styles/main.module.css";
 
 export default class Menu extends React.Component {
   state = {
-    titlesList: ["Articles", "Meditation", "Sleep", "Stress", "Mindfulness"],
-    aboutList: ["Work", "About", "Help", "Login"],
+    titlesList: ["Techniques", "Chat"],
+    aboutList: ["Register", "Login", "About"],
   };
 
   renderMenuItems = (list) => {
     return list.map(function (itemTitle) {
+      let lowerCaseTitle = itemTitle.toLowerCase();
       return (
         <React.Fragment key={itemTitle}>
-          <li className={`${styles["menu__item"]}`}>
-            <p className={`${styles["menu__item__title"]}`}>{itemTitle}</p>
-            <Arrow />
-          </li>
+          <NavLink to={lowerCaseTitle}>
+            <li className={`${styles["menu__item"]}`}>
+              <p className={`${styles["menu__item__title"]}`}>{itemTitle}</p>
+              <Arrow />
+            </li>
+          </NavLink>
         </React.Fragment>
-
       );
     });
   };
@@ -28,12 +31,12 @@ export default class Menu extends React.Component {
     return (
       <section className={`${styles["menu"]}`}>
         <div>
-          <h3 className={`${styles["menu__header"]}`}>Browse articles</h3>
+          <h3 className={`${styles["menu__header"]}`}>LetsTalk</h3>
           <ul>{this.renderMenuItems(this.state.titlesList)}</ul>
         </div>
 
         <div>
-          <h3 className={`${styles["menu__header"]}`}>About LetsTalk</h3>
+          <h3 className={`${styles["menu__header"]}`}>Extras</h3>
           <ul>{this.renderMenuItems(this.state.aboutList)}</ul>
         </div>
       </section>
