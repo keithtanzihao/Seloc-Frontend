81bbebb726285f9dc22e79395daea7cadcc3f0c1
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:22:49 +0800

    TechniqueSearch: Moved search functions from Technique.js here, re-implemented html code

--- src/components/layout/technique/TechniqueSearch.js
+++ src/components/layout/technique/TechniqueSearch.js
@@ -1,21 +1,53 @@
 import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
 
 import styles from "../../../styles/main.module.scss";
+import { BsSearch } from "react-icons/bs";
+
+let searchTimer;
+const BASE_API_URL = "http://localhost:3001/";
 
 export default class TechniqueSearch extends React.Component {
-  
+  state = {
+    filterOptions: this.props.filterOptions || {},
+  };
+
+  searchFieldSubmit = async () => {
+    let queryPayload = this.props.searchField;
+    let filterPayload = JSON.stringify({
+      ...this.props.filterOptions,
+    });
+
+    let response = await axios.get(
+      BASE_API_URL + `techniques/search/${queryPayload}/${filterPayload}`
+    );
+    console.log(response);
+  };
+
   render() {
+    const { searchField } = this.props;
     return (
-      <section className={`${styles["technique__searchCtn"]}`}>
-        <input
-          className={`${styles["technique__search"]}`}
-          type="text"
-          name="searchField"
-          value={this.props.searchField}
-          onChange={this.props.searchFieldUpdate}
-          placeholder="Search"
-        />
-      </section>
+      <div className={`${styles["technique__searchCtn"]}`}>
+        <div>
+          <input
+            className={`${styles["technique__searchBar"]}`}
+            type="text"
+            name="searchField"
+            value={searchField}
+            onChange={(event) =>
+              this.props.updateStateObjects("searchField", event.target.value)
+            }
+            placeholder="Search"
+          />
+          <button
+            className={`${styles["technique__searchBtn"]}`}
+            onClick={this.searchFieldSubmit}
+          >
+            <BsSearch/>
+          </button>
+        </div>
+      </div>
     );
   }
 }
