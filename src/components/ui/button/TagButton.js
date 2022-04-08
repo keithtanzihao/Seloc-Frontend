eb0915a26003b33b9c1b22f8f3b30facd7b1f2d3
Keith Tan <keithtanzihao@gmail.com>
2022-04-08 20:26:35 +0800

    button/TagButton: Changed classNames, span tag, removed state

--- src/components/ui/button/TagButton.js
+++ src/components/ui/button/TagButton.js
@@ -3,34 +3,29 @@ import React from "react";
 import styles from "../../../styles/main.module.css";
 
 export default class TagButton extends React.Component {
-
-  state = {
-    isChecked: false
-  }
-
-  updateIsChecked = () => {
-    this.setState({
-      isChecked: !this.state.isChecked
-    })
-  }
-
   render() {
+    const { tag, className, updateSelectedTags, checked } = this.props;
+
     return (
-      <div className={`${styles["tagBtn"]}`}>
-        <label className={`${styles["tagBtn__content"]}`}>
+      <div
+        className={
+          checked
+            ? `${styles["tagBtn"]} ${styles["checked"]}`
+            : `${styles["tagBtn"]}`
+        }
+      >
+        <label className={`${styles["tagBtn__content"]} `}>
           <input
-            className={this.props.className}
+            className={className}
             type="checkbox"
-            name={this.props.category}
-            value={this.props.category}
-            onChange={this.props.onChange}
-            checked={this.props.checked}
+            name={tag}
+            value={tag}
+            onChange={updateSelectedTags}
+            checked={checked}
           />
-          <span className={(this.props.checked) ?
-              `${styles["tagBtn__title"]} ${styles["checked"]}` :
-              `${styles["tagBtn__title"]}`
-            }>
-            {this.props.category}
+          <span className={`${styles["tagBtn__title"]}`}>
+            {/* Output tag btns */}
+            {tag}
           </span>
         </label>
       </div>
