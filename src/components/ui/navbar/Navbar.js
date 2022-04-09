ea930bc29d948a3e0f470ca38f7b944fb4ff261a
Keith Tan <keithtanzihao@gmail.com>
2022-04-09 23:30:37 +0800

    navbar/Navbar.js: Re-added back menu button, set menu button position

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -4,27 +4,28 @@ import { NavLink } from "react-router-dom";
 import NavbarMenu from "./NavbarMenu";
 
 import styles from "../../../styles/main.module.css";
-import loginImg from "../../../styles/vendors/imgs/login/loginImg.png";
 
 import { IconContext } from "react-icons";
 import {
-  BsFillHouseDoorFill,
   BsFillBookFill,
   BsFillPersonFill,
 } from "react-icons/bs";
 
 export default class Navbar extends React.Component {
 
-  componentDidMount = () => {
-    
-  }
-
   render() {
     return (
       <nav className={`${styles["navbar"]}`}>
         <div className={`${styles["navbar__ctn"]}`}>
-          <h1 className={`${styles["navbar__title"]}`}>BetterLife</h1>
+
+          <div>
+            <NavbarMenu isMenuOpen={this.props.isMenuOpen} setIsMenuOpen={this.props.setIsMenuOpen} />
+            <h1 className={`${styles["navbar__title"]}`}>BetterLife</h1>
+          </div>
+
           <ul className={`${styles["navbar__list"]}`}>
+
+
             <IconContext.Provider
               value={{ className: `${styles["navbar__icons"]}` }}
             >
@@ -42,7 +43,7 @@ export default class Navbar extends React.Component {
               </NavLink>
             </IconContext.Provider>
 
-            {/* <NavbarMenu setIsMenuOpen={this.props.setIsMenuOpen} /> */}
+
           </ul>
         </div>
       </nav>
