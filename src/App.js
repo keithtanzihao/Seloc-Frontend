929f71616ddbd2f88c7917b30b6c5cdcb29345dc
Keith Tan <keithtanzihao@gmail.com>
2022-04-10 16:25:02 +0800

    App.js: Updated react-router paths to better link to pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Navbar from "./components/ui/navbar/Navbar";
 import Menu from "./components/ui/menu/Menu";
-import Frontpage from "./components/layout/Frontpage";
-import Signup from "./components/layout/Signup";
+import Login from "./components/layout/Login";
 import Techniques from "./components/layout/Techniques";
+import TechniqueAdd from "./components/layout/technique/TechniqueAdd";
+import TechniqueDisplay from "./components/ui/modal/TechniqueDisplay";
+
+import Register from "./components/layout/Register";
 
 import "./styles/main.module.scss";
-
 
 
 export default class App extends React.Component {
   state = {
     isMenuOpen: false,
+    isLoggedIn: false,
+    userEmail: ""
   };
 
   setIsMenuOpen = () => {
@@ -30,18 +34,54 @@ export default class App extends React.Component {
     }
   };
 
+  renderContentTest = () => {
+    if (this.state.isMenuOpen) {
+      return <Menu setIsMenuOpen={this.setIsMenuOpen}/>;
+    }
+    return;
+  };
+
   render() {
     return (
       <React.Fragment>
         {/* Common components here */}
-        <Navbar setIsMenuOpen={this.setIsMenuOpen} />
 
-        <Route path="/main">{this.renderContent(<Frontpage />)}</Route>
-        <Route path="/login">{this.renderContent(<Signup authType="login" />)}</Route>
-        <Route path="/signup">{this.renderContent(<Signup authType="signup" />)}</Route>
-        <Route path="/articles">{this.renderContent(<Techniques />)}</Route>
+        <Navbar isMenuOpen={this.state.isMenuOpen} setIsMenuOpen={this.setIsMenuOpen} />
+        {this.renderContentTest()}
 
+        <Switch>
+          <Route path="/login"><Login /></Route>
+          <Route path="/register"><Register /></Route>
+
+
+          <Route path="/technique/:id"><TechniqueDisplay /></Route>
+
+          <Route path="/techniques/add-technique"><TechniqueAdd /></Route>
+          <Route path="/techniques/edit-technique"><TechniqueAdd /></Route>
+
+          <Route path="/techniques"><Techniques /></Route>
+          
+        </Switch>
+      
       </React.Fragment>
     );
   }
 }
+
+
+
+
+/**
+ * Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis itaque dolore ut dicta sit vel enim? Soluta expedita sunt repellendus atque officia commodi aut eius, ab fuga sequi a. Consectetur.
+ * 
+ * https://media.istockphoto.com/photos/singapore-skyline-at-marina-bay-at-twilight-with-glowing-sunset-the-picture-id1176969551?b=1&k=20&m=1176969551&s=170667a&w=0&h=JvC68A7XNAH-C4CiPFHHNWQM4GeAN9E1me-vcyF_MWg=
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * KIV must add error message if theres neither 1 insturction/benefit for techniqueAddInstructBen
+ * 
+ * Remove br from techniqueDisplay later at difficulty:
+ */
